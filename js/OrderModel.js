6f59cd4d976e6cd56882fae646ee74b852a813ea
Dmitriy Lyubimov <d.lubimov@abbyy-ls.com>
2015-04-03 17:09:20 +0300

    refactoring

---
+++ js/OrderModel.js
@@ -0,0 +1,258 @@
+/**
+ * Created by d.lubimov on 03.04.2015.
+ * @module
+ */
+/** @class */
+function OrderModel() {
+	/** @type {string} */
+	this.username = "";
+	/** @type {string[]} */
+	this._allUsers = [];
+	/** @type {OrderItem[]} */
+	this.itemsList = [];
+	/** @type {CoffeeModel[]} */
+	this.coffeeList = [];
+
+	this._eventsTarget = $(this);
+}
+
+/** @type {number[]} */
+OrderModel.prototype.weightsList = [
+	150,
+	450
+];
+
+OrderModel.prototype.fire = function() {
+	return this._eventsTarget.trigger.apply(this._eventsTarget, arguments);
+};
+
+OrderModel.prototype.on = function() {
+	return this._eventsTarget.on.apply(this._eventsTarget, arguments);
+};
+
+/** @param {string} username */
+OrderModel.prototype.setUserName = function(username) {
+	this.username = username;
+	this.saveToStorage();
+	this.fire("username-changed", this.username);
+};
+
+OrderModel.prototype.applyCoffeeList = function(list) {
+	list = $.map(list, function(coffeeData) {
+		var coffee = new CoffeeModel();
+		coffee.id = coffeeData["id"];
+		coffee.name = coffeeData["name"];
+		coffee.subName = coffeeData["subName"];
+		coffee.price = coffeeData["price"];
+		return coffee;
+	});
+
+	list.sort(function (/**CoffeeModel*/i1, /**CoffeeModel*/i2) {
+		if (i1.name == i2.name) {
+			return i1.subName > i2.subName ? 1 : -1;
+		}
+		return i1.name > i2.name ? 1 : -1;
+	});
+
+	this.coffeeList = list;
+
+	// появились настоящие объекты кофе, пора прикрутить на них ссылки
+	for (var i = 0; i < this.itemsList.length; ++i) {
+		var item = this.itemsList[i];
+		item.coffee = this.getCoffee(item.id);
+	}
+
+	this.fire("coffee-list-changed", this.coffeeList);
+};
+
+OrderModel.prototype.clearItems = function () {
+	this.itemsList = [];
+	this.saveToStorage();
+	this.fire("items-list-changed", this.itemsList);
+};
+
+/**
+ * @param {CoffeeModel} coffee
+ * @param {number} weight
+ * @returns {OrderItem}
+ */
+OrderModel.prototype.getItem = function (coffee, weight) {
+	for (var i = 0; i < this.itemsList.length; i++) {
+		var orderItem = this.itemsList[i];
+		if (orderItem.coffee == coffee) {
+			if (orderItem.weight == weight) {
+				return orderItem;
+			}
+		}
+	}
+	return null;
+};
+
+/**
+ * @param {CoffeeModel} coffee
+ * @param {number} weight
+ * @returns {OrderItem}
+ */
+OrderModel.prototype.addQuantity = function(coffee, weight) {
+	var currentItem = this.getItem(coffee, weight);
+	if (!currentItem) {
+		currentItem = new OrderItem();
+		currentItem.coffee = coffee;
+		currentItem.id = coffee.id;
+		currentItem.weight = weight;
+		this.itemsList.push(currentItem);
+	}
+	currentItem.quantity++;
+
+	this.saveToStorage();
+	this.fire("item-changed", currentItem);
+	return currentItem;
+};
+
+/**
+ * @param {CoffeeModel} coffee
+ * @param {number} weight
+ * @returns {OrderItem}
+ */
+OrderModel.prototype.substractQuantity = function(coffee, weight) {
+	var currentItem = this.getItem(coffee, weight);
+	if (!currentItem) {
+		return currentItem;
+	}
+	currentItem.quantity--;
+
+	this.saveToStorage();
+	this.fire("item-changed", currentItem);
+	return currentItem
+};
+
+OrderModel.prototype.loadFromStorage = function() {
+	this.username = localStorage.getItem("username");
+	var dataStr = localStorage.getItem("order");
+	if (!dataStr) {
+		return;
+	}
+
+	var data = JSON.parse(dataStr);
+	this.username = data["username"];
+	var list = data["list"];
+
+	for (var i = 0; i < list.length; i++) {
+		var itemData = list[i];
+		var item = new OrderItem();
+		item.id = itemData["id"];
+		item.quantity = itemData["quantity"];
+		item.weight = itemData["weight"];
+		this.itemsList.push(item);
+	}
+	this.fire("load");
+};
+
+OrderModel.prototype.saveToStorage = function() {
+	var data = {};
+	data["username"] = this.username;
+	data["list"] = [];
+
+	for (var i = 0; i < this.itemsList.length; i++) {
+		var item = this.itemsList[i];
+		data["list"].push({
+			"id": item.id,
+			"quantity": item.quantity,
+			"weight": item.weight,
+		});
+	}
+
+	localStorage.setItem("order", JSON.stringify(data));
+};
+
+/** @returns {number} */
+OrderModel.prototype.getTotal = function() {
+	var total = 0;
+
+	for (var i = 0; i < this.itemsList.length; i++) {
+		var item = this.itemsList[i];
+		var coffee = this.getCoffee(item.id);
+		if (!coffee) {
+			continue;
+		}
+		var price = coffee.price[item.weight];
+		total += price * item.quantity;
+	}
+
+	return total;
+};
+
+/**
+ * @param {string} id
+ * @returns {CoffeeModel}
+ */
+OrderModel.prototype.getCoffee = function(id) {
+	for (var i = 0; i < this.coffeeList.length; i++) {
+		var coffee = this.coffeeList[i];
+		if (coffee.id == id) {
+			return coffee;
+		}
+	}
+
+	return null;
+};
+
+/** @returns {Object} */
+OrderModel.prototype.getRequestData = function() {
+	this.saveToStorage();
+	var data = {"order": localStorage.getItem("order")};
+	return data;
+};
+
+/** @param {OrderModel} order */
+OrderModel.prototype.addOrder = function(order) {
+	this._allUsers.push(order.username);
+
+	for (var i = 0; i < order.itemsList.length; ++i) {
+		var otherItem = order.itemsList[i];
+		var item = this.getItem(otherItem.coffee, otherItem.weight);
+		if (!item) {
+			item = otherItem.clone();
+		} else {
+			item.quantity += otherItem.quantity;
+		}
+	}
+};
+
+
+
+
+/** @class */
+function CoffeeModel() {
+	/** @type {string} */
+	this.id = "";
+	/** @type {string} */
+	this.name = "";
+	/** @type {string} */
+	this.subName = "";
+	/** @type {number[]} */
+	this.price = "";
+}
+
+/** @class */
+function OrderItem() {
+	/** @type {CoffeeModel} */
+	this.coffee = null;
+	/** @type {string} */
+	this.id = "";
+	/** @type {number} */
+	this.weight = "";
+	/** @type {number} */
+	this.quantity = 0;
+}
+
+/** @returns {OrderItem} */
+OrderItem.prototype.clone = function() {
+	var clone = new OrderItem();
+	clone.coffee = this.coffee;
+	clone.id = this.id;
+	clone.quantity = this.quantity;
+	clone.weight = this.weight;
+
+	return clone;
+};
---
+++ js/script.js
@@ -0,0 +1,133 @@
+/**
+ * Created by d.lubimov on 03.04.2015.
+ */
+
+var order = new OrderModel();
+var table;
+
+order.on("coffee-list-changed", drawCoffeeTable);
+order.on("items-list-changed", drawCoffeeTable);
+
+
+$(function () {
+	table = $("#coffee-table");
+	order.loadFromStorage();
+	$("#username")
+		.change(function () {
+			order.setUserName(this.value.trim());
+		})
+		.val(order.username);
+
+	$("#reset").click(function() {
+		order.clearItems();
+	});
+
+	$("#submit-order").click(function() {
+		var url = "/submit-order/";
+		var data = order.getRequestData();
+		$.post(url, data);
+	});
+
+	table
+		.on("click", ".quantity .add", function () {
+			addQuantity(this);
+		})
+		.on("click", ".quantity .substract", function () {
+			substractQuantity(this);
+		});
+
+	$.getJSON("data.json").done(function (list) {
+		order.applyCoffeeList(list);
+	});
+});
+
+
+function drawCoffeeTable() {
+	var list = order.coffeeList;
+
+	var tbody = table.find("tbody");
+	tbody.html("");
+
+	for (var i = 0; i < list.length; i++) {
+		var row = $("<tr/>");
+
+		var coffee = list[i];
+		var name = coffee.name;
+		if (coffee.subName) {
+			name += " (" + coffee.subName + ")";
+		}
+
+		row.append("<td><a href='" + coffee.link + "'>" + name + "</a></td>");
+		for (var j = 0; j < order.weightsList.length; j++) {
+			var weight = order.weightsList[j];
+			row.append("<td class='price'>" + coffee.price[weight] + " р.</td>");
+			row.append($("<td></td>").append(createQuantityContent(coffee, weight)));
+		}
+		tbody.append(row);
+	}
+	setTotal();
+}
+
+/**
+ * @param {CoffeeModel} coffee
+ * @param {number} weight
+ * @param {OrderItem} [item]
+ * @returns {HTMLElement}
+ */
+function createQuantityContent(coffee, weight, item) {
+	item = item || order.getItem(coffee, weight);
+	var quantity = item ? item.quantity : 0;
+	var content = $("<div class='quantity'></div>");
+	content.attr("coffee-id", coffee.id);
+	content.attr("weight", weight);
+	content.append("<span>" + quantity + "</span>");
+	content.append("<button class='add'>want it!</button>");
+	if (quantity > 0) {
+		content.append("<button class='substract'>fck it!</button>");
+	}
+
+	content.data("coffee", coffee);
+	content.data("weight", weight);
+
+	return content;
+}
+
+/** @param {HTMLElement} button */
+function addQuantity(button) {
+	var quantity = $(button).parents(".quantity");
+	var coffee = quantity.data("coffee");
+	var weight = quantity.data("weight");
+	var item = order.addQuantity(coffee, weight);
+	redrawQuantity(item);
+}
+
+/** @param {HTMLElement} button */
+function substractQuantity(button) {
+	var quantity = $(button).parents(".quantity");
+	var coffee = quantity.data("coffee");
+	var weight = quantity.data("weight");
+	var item = order.substractQuantity(coffee, weight);
+	redrawQuantity(item);
+}
+
+function redrawAllQuantities() {
+	for (var i = 0; i < order.itemsList.length; ++i) {
+		var item = order.itemsList[i];
+		redrawQuantity(item);
+	}
+}
+/**
+ * @param {OrderItem} item
+ */
+function redrawQuantity(item) {
+	if (!item) {
+		return;
+	}
+	var quantity = table.find(".quantity[coffee-id='" + item.id + "'][weight='" + item.weight + "']");
+	quantity.replaceWith(createQuantityContent(item.coffee, item.weight));
+	setTotal();
+}
+
+function setTotal() {
+	$("#total").text(order.getTotal());
+}
